dec70fcab2e2e91cdd68aafea8b06bfd98ead23f
yaneyalvarado <alvyaney@gmail.com>
2023-03-10 13:23:24 -0700

    added questions to JS

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,22 @@
 var questions = [{
-    question: "JavaScript is an _____________ language?" ,
-    options: ["A: Object-Oriented", "B: Object-Based" , "C: Procedural" , "D: None of the above"] ,
-    answer: "a-2"
+    question: "JavaScript is a _____________ language?" ,
+    options: ["A: Markup language", "B: Style" , "C: Procedural" , "D: Scripting"] ,
+    answer: "D: JavaScript is a scripting language."
 
 },{
-    question: "Example question 2" ,
-    options: ["aa", "ab" , "ac" , "ad"] ,
-    answer: "ab"
+    question: "Inside which HTML element do we put the JavaScript?" ,
+    options: ["A: <script>", "B: <scripting>" , "C: <javascript>" , "D: <js>"] ,
+    answer: "A: <script>"
+
+},{
+    question: "Where is the correct place to insert a JavaScript?" ,
+    options: ["A: The <head> section", "B: Both the <head> section and the <body> section are correct" , "C: The <body> section" , "D: None of the above"] ,
+    answer: "C: JavaScript is inserted in the <body> section"
+},{
+    question: "How do you create a function in JavaScript?" ,
+    options: ["A: function myFunction()", "B: function:myFunction()" , "C: function = myFunction()" , "D: function myFunction[] "] ,
+    answer: "A: function myFunction()"
+
 
 }]
 var questionCount = 0
